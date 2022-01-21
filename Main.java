fe090415992120fda4b6dc0386e5ac7c48277926
ButWhatIf <97361632+ButWhatIf@users.noreply.github.com>
2022-01-20 22:24:39 -0700

    Add files via upload
    Example model

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // Creating a MLP with 1 input, 1 output, and 2 hidden layers with 64 and 32 nodes respectively
+        Sequential toy = new Sequential(1, 64, 32, 1);
+        // Setting activations for the hidden and output layers
+        toy.setActivations(new Sigmoid(), new Sigmoid(), new Linear());
+        // Loading the training dataset
+        toy.importData("C:\\IdeaProjects\\mydata.txt");
+
+        /*
+        Training the MLP for 50,000 batches using a mini-batch momentum gradient descent with learning rate
+        0.001, momentum decay rate of 0.90, and batch size of 10, evaluated with mean average error.
+         */
+        toy.train(50000, new Momentum(0.001, 0.90), 10, new MeanAverageError());
+
+        // Writing the predicted outputs using this.predict() to a file to compare
+        PrintWriter printer = new PrintWriter("C:\\IdeaProjects\\tests.txt");
+        for (int i = -10; i < 10; i++) {
+            printer.println((double) i/2 + "," + toy.predict(Double.toString((double) i / 2)));
+        }
+        printer.close();
+    }
+}
